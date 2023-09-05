73b156feaea7db8f4b10c89c9de838d6f4f9534e
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2023-09-05 16:16:32 -0500

    commit 2: Primitive Datatypes1.

--- src/test2/NewFrame.java
+++ src/test2/NewFrame.java
@@ -5,6 +5,32 @@ public class NewFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New class created.");
+		//This is a comment
+		
+		int number = 100;
+		double decimal = 100.01;
+		String brand = "Nike";
+		//Date date = new Date();
+		
+		
+		//primitive Datatypes:
+		
+		byte theByte = -128; // byte stores whole # from -128 to 127
+		short theShort = 9_379; // short whole numbers from -32768 to 32767
+		int theInt = 297_824_676;// int whole numbers from -2147483648 to 2147483647
+		long theLong = 9_874_983_945_785_745L; // long stores whole # from -128 to 127
+		float theFloat = 891.8_574_584F; // float stores 6 to 7 decimal
+		double theDouble = 38.38475736; // double stores 15 decimals
+		boolean isAdult = true;
+		char nameInitial = 'A';
+		
+		System.out.println(theByte);
+		System.out.println(theShort);
+		System.out.println(theInt);
+		System.out.println(theLong);
+		System.out.println(theFloat);
+		System.out.println(theDouble);
+		System.out.println(isAdult);
 	}
 
 }
