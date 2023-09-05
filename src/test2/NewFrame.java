ae13dedef13810c5c9b5b6086799d85f1bac0ea4
busugu2018 <37343610+busugu2018@users.noreply.github.com>
2023-09-05 15:36:13 -0500

    commit 1: Initial commit.

---
+++ src/test2/NewFrame.java
@@ -0,0 +1,10 @@
+package test2;
+
+public class NewFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New class created.");
+	}
+
+}
---
+++ src/test2/main.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hey Guys");
+		System.out.println("I'm back to learning Java.");
+	}
+
+}
